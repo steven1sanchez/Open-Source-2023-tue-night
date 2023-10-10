b0a22b7784e3f3e523df8b526fecf0cfb5df75a8
Jonathan Beck <JJBeck7@gmail.com>
2023-10-10 18:38:06 -0500

    Completed 2 coding bat files

--- src/main/java/com/java2/opensource2023tuenight/Map2.java
+++ src/main/java/com/java2/opensource2023tuenight/Map2.java
@@ -18,4 +18,27 @@ public class Map2 {
         return map;
     }
 
+    public String[] allSwap__Jonathan_Beck(String[] strings) {
+        Map<String, Integer> map2 = new HashMap<String, Integer>();
+        for (int i = 0; i < strings.length; i++) {
+            String key_char = String.valueOf(strings[i].charAt(0));
+
+            if (map2.containsKey(key_char)) {
+
+                Integer index_to_swap = map2.get(key_char);
+                String temp = strings[index_to_swap];
+                strings[index_to_swap]= strings[i];
+                strings[i]=temp;
+
+                map2.remove(key_char);
+
+            }
+            else {
+                map2.put(key_char,i);
+            }
+        }
+
+        return strings;
+    }
+
 }
