9321d9771bca903be9cf0a8e7a9619556851e090
Baylee Gabrielson <baygabe234@gmail.com>
2023-10-10 18:30:13 -0500

    Added my version of firstSwap into Map2 and mapAB4 into Map1

--- src/main/java/com/java2/opensource2023tuenight/Map1.java
+++ src/main/java/com/java2/opensource2023tuenight/Map1.java
@@ -10,4 +10,17 @@ public class Map1 {
         return map;
     }
 
+    public static Map<String, String> mapAB4_baylee(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() == map.get("b").length()){
+                map.put("a", "");
+                map.put("b", "");
+            } else if (map.get("a").length() >= map.get("b").length()) {
+                map.put("c", map.get("a"));
+            } else{
+                map.put("c", map.get("b"));
+            }
+        }
+        return map;
+    }
 }
