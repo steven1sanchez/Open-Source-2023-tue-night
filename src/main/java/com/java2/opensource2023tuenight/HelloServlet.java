43cc02ab80819cb139eaa6a59cb064988d46dd00
Jacob Wendt <jacob.wendt2@gmail.com>
2023-10-10 18:13:57 -0500

    added haikus

--- src/main/java/com/java2/opensource2023tuenight/HelloServlet.java
+++ src/main/java/com/java2/opensource2023tuenight/HelloServlet.java
@@ -9,9 +9,7 @@ import jakarta.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    public void init() {message = "Hello World!";}
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
