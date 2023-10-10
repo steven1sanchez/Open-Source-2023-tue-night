8f9edf20584a8b0271caae9db337889ed831606c
Nathan Toothaker <nathan-toothaker@student.kirkwood.edu>
2023-10-10 18:34:51 -0500

    added map1.mapAB4_Nathan and map2.firstSwap_Nathan

--- src/main/java/com/java2/opensource2023tuenight/Map1.java
+++ src/main/java/com/java2/opensource2023tuenight/Map1.java
@@ -3,6 +3,12 @@ package com.java2.opensource2023tuenight;
 import java.util.Map;
 
 public class Map1 {
+    public static Map<String, String> mapAB_marc(Map<String, String> map) {
+        if(map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
     public static Map<String, String> mapAB4_Nathan(Map<String, String> map) {
         if(map.containsKey("a") && map.containsKey("b")){
             if(map.get("a").length() == map.get("b").length()){
--- src/main/java/com/java2/opensource2023tuenight/Map2.java
+++ src/main/java/com/java2/opensource2023tuenight/Map2.java
@@ -4,6 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
+    public static Map<String, Boolean> wordMultiple_marc(String[] strings) {
+        Map<String, Boolean> map = new HashMap<>();
+        for(String str: strings){
+            if(!map.containsKey(str)){
+                map.put(str, false);
+            }
+            else{
+                map.put(str, true);
+            }
+        }
+        return map;
+    }
+
     public String[] firstSwap(String[] strings) {
         Map<String, Integer> map = new HashMap<>();
         String[] outStrings = new String[strings.length];
