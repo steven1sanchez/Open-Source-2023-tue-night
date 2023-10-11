b21092ab4aaa497d65a56dedd425b3b61ec87d50
Jacob Lindauer <jadlindauer@gmail.com>
2023-10-10 19:11:55 -0500

    Added CodingBat methods to Map files

--- src/main/java/com/java2/opensource2023tuenight/Map2.java
+++ src/main/java/com/java2/opensource2023tuenight/Map2.java
@@ -18,4 +18,13 @@ public class Map2 {
         return map;
     }
 
+    public Map<String, String> pairs_jacobL(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String str : strings){
+            map.put(str.substring(0, 1), str.substring(str.length()-1, str.length()));
+        }
+
+        return map;
+    }
+
 }
