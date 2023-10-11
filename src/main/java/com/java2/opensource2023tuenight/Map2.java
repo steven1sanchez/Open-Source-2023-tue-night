a42d73ae9fbf38368f3c2603c67e168537485e64
Caspian Wilson <caspian-wilson@student.kirkwood.edu>
2023-10-10 19:03:51 -0500

    add map1 and map2

---
+++ src/main/java/com/java2/opensource2023tuenight/Map2.java
@@ -0,0 +1,15 @@
+package com.java2.opensource2023tuenight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map2 {
+    public Map<String, Integer> wordLen_caspian(String[] strings) {
+        Map<String, Integer> stringCount = new HashMap<>();
+        for (String add : strings) {
+            int num = add.length();
+            stringCount.put(add, num);
+        }
+        return stringCount;
+    }
+}
