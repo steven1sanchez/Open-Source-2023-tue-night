476e74e65fbc9272f69d30f39f0edecd3caf2bb7
Jacob Lindauer <jadlindauer@gmail.com>
2023-10-17 18:57:06 -0500

    Made class static, applied unit test for Map2

--- src/main/java/com/java2/opensource2023tuenight/Map2.java
+++ src/main/java/com/java2/opensource2023tuenight/Map2.java
@@ -33,7 +33,7 @@ public class Map2 {
     }
 
 
-    public Map<String, String> pairs_jacobL(String[] strings) {
+    public static Map<String, String> pairs_jacobL(String[] strings) {
         Map<String, String> map = new HashMap<>();
         for (String str : strings){
             map.put(str.substring(0, 1), str.substring(str.length()-1, str.length()));
