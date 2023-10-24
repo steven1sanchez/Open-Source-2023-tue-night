f229876d11ca2c6c2919ff614235b5ccc0fb92a7
Caspian Wilson <caspian-wilson@student.kirkwood.edu>
2023-10-24 09:45:03 -0500

    Added Caspian's Map1 Test

--- src/main/java/com/java2/opensource2023tuenight/Map1.java
+++ src/main/java/com/java2/opensource2023tuenight/Map1.java
@@ -47,7 +47,7 @@ public class Map1 {
         return map;
     }
 
-    public Map<String, String> mapAB2_caspian(Map<String, String> map) {
+    public static Map<String, String> mapAB2_caspian(Map<String, String> map) {
         if(map.containsKey("a") && map.containsKey("b")) {
             if(map.get("a").equals(map.get("b"))) {
                 map.remove("a");
